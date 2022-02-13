41609a335a89f13f11ba54bbeaf09cf9cd205bd6
Sounak Roy <sounakroy.t@ycs.in>
2022-02-13 20:21:21 +0530

    Develop boiler plate of Next Js Yethi Project

--- pages/users/index.js
+++ pages/users/index.js
@@ -1,5 +1,13 @@
+import Link from "next/link";
 const Users = () => {
-  return <div>This is users page</div>;
+  return (
+    <div>
+      This is users page
+      <Link href="https://github.com/RoySounak/nextYethi">
+        <h2 style={{ cursor: "pointer" }}>Go to Sourec Code</h2>
+      </Link>
+    </div>
+  );
 };
 
 export default Users;
